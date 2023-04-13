4fc91dce78f27170f827d4c8dae4a3b6a6b11548
jahiker <rojasjahiker@gmail.com>
2023-04-13 18:06:59 -0400

    practica react memo y usecallback

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -2,17 +2,18 @@ import { useState, useCallback } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
+
   const [counter, setCounter] = useState(0);
 
-  const increment = useCallback(() => {
-    setCounter(value => value + 1);
+  const incrementFather = useCallback((value = 1) => {
+    setCounter(c => c + value);
   }, []);
 
   return (
     <>
       <h1>useCallback Hook: {counter}</h1>
       <hr />
-      <ShowIncrement increment={increment} />
+      <ShowIncrement increment={incrementFather} />
     </>
   );
 };
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 export const ShowIncrement = React.memo(({ increment }) => {
   console.log("Me volvi a generar");
+
   return (
-    <>
-      <button className="btn btn-primary" onClick={increment}>
-        +1
-      </button>
-    </>
+    <button className="btn btn-primary" onClick={() => increment(5)}>
+      Incrementar
+    </button>
   );
 });
