e444e00fc3270de45deeee2592efe955898f7342
jahiker <rojasjahiker@gmail.com>
2023-04-13 23:12:39 -0400

    reducer

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import "./index.css";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { CallbackHook } from "./06-memos/CallbackHook";
+import './08-useReducer/introReducer'
 import { Padre } from "./07-tarea-memo/Padre";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
